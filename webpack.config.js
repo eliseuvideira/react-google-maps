78a952935cd18adc8f87052c1c4bbd198a6f8b87
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-12 14:00:02 -0300

    added dotenv to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -36,5 +37,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
     }),
+    new Dotenv(),
   ],
 };
