eefad0c7983510473423512d74529466f6394fa7
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-11 21:15:01 -0300

    initial setup for react app

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        ],
+        resolve: {
+          extensions: ['.js', '.jsx', '.json'],
+        },
+      },
+    ],
+  },
+};
